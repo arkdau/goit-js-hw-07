c042f4d7f9e999039dbe7ecf11b812765cc1bb10
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-09 01:18:29 +0200

    add option to SimpleLightbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,7 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const list = document.querySelector('.gallery');
-const markup = galleryItems.map((item) => `<li><a  class="gallery__item" href=${item.original}><img  class="gallery__image"  src=${item.preview} alt=${item.description} width="480"  height=auto></a></li>`);
+const markup = galleryItems.map((item) => `<li><a  class="gallery__item" href=${item.original}><img  class="gallery__image"  src=${item.preview} alt="${item.description}" width="480"  height=auto></a></li>`);
 list.insertAdjacentHTML('beforeend', markup);
+console.log()
 
-const gallery = new SimpleLightbox('.gallery a');
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionPosition: 'outside',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: true,
+});
