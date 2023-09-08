ce4784977d0c791aee00a28e8da55f00efebc56e
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-08 22:31:39 +0200

    add -  SimpleLightbox - constuctor

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector('.gallery');
+const markup = galleryItems.map((item) => `<li><a  class="gallery__item" href=${item.original}><img  class="gallery__image"  src=${item.preview} alt=${item.description} width="480"  height=auto></a></li>`);
+list.insertAdjacentHTML('beforeend', markup);
 
-console.log(galleryItems);
+const gallery = new SimpleLightbox('.gallery a');
