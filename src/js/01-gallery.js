3ddd3ef5f6cdcfab3675195a95358ccf4f4a3a8a
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-10 00:27:23 +0200

    feat: add class: ClassLightBox{}

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,51 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // import * as basicLightbox from '../basiclightbox.js';
 
 // Change code below this line
-class gallery {
-}
-let controller;
-let instance = null;
+class ClassLightBox {
+  instance;
 
-// Close modal window
-function escspePress(e) {
-  e.preventDefault();
-  console.log('enter escapePress');
-  if (e.key === 'Escape') {
-    instance.close();
-    console.log(this);
-    controller.abort(); // remove listener
-    console.log('event listener was removed from body, aborted = ', controller.signal.aborted);
+  controller;
+
+  constructor() { }
+
+  // Close modal window
+  escspePress = (e) => {
+    e.preventDefault();
+    if (e.key === 'Escape') {
+      this.instance.close();
+      // console.log(this);
+      this.controller.abort(); // remove listener
+      console.log(
+        'event listener was removed from body (keydown-Escape), aborted = ',
+        this.controller.signal.aborted,
+      );
+    }
+  };
+
+  create(e) {
+    e.preventDefault(); // Stop the default action
+    const elem = e.target;
+    const src = elem.dataset.source;
+    console.log(src);
+
+    if (elem.nodeName === 'IMG') {
+      // create modal
+      this.instance = basicLightbox.create(`
+         <img src=${src} width="1280" height=auto>
+       `);
+      this.instance.show();
+      this.controller = new AbortController();
+      document.body.addEventListener('keydown', this.escspePress, {
+        signal: this.controller.signal,
+      });
+    }
   }
 }
 
 const list = document.querySelector('.gallery');
-const markup = galleryItems.map((item) => `<li class="gallery__item"><a  class="gallery__link" href=${item.original}><img  class="gallery__image"  src=${item.preview} data-source=${item.original} alt="${item.description}" width="480"  height=auto></a></li>`).join('');
-
+const markup = galleryItems.map((item) =>
+  `<li class="gallery__item"><a  class="gallery__link" href=${item.original}><img  class="gallery__image"  src=${item.preview} data-source=${item.original} alt="${item.description}" width="480"  height=auto></a></li>`).join('');
 list.insertAdjacentHTML('beforeend', markup);
-list.addEventListener('click', (event) => {
-  event.preventDefault(); // Stop the default action
-  const elem = event.target;
-  // console.log(elem.currentSrc);
-  // let src = '';
-  const src = elem.dataset.source;
-  console.log(src);
 
-  if (elem.nodeName === 'IMG') {
-  // Second metod
-  //   const li = elem.closest('li'); // get reference
-  //   const nodes = Array.from(li.closest('ul').children); // get Array
-  //   const index = nodes.indexOf((li));
-  //   console.log(index);
-  //   console.log(elem);
-  //   src = galleryItems[index].original;
-
-    // create modal
-    instance = basicLightbox.create(`
-         <img src=${src} width="1280" height=auto>
-       `);
-    instance.show();
-    controller = new AbortController();
-    document.body.addEventListener('keydown', escspePress, { signal: controller.signal });
-  }
-});
+const imgLightBox = new ClassLightBox();
+list.addEventListener('click', imgLightBox.create.bind(imgLightBox));
